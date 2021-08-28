1c2d0ff716fca87c222e298e0487d60a5d35ff5f
Jasleen001 <88023368+Jasleen001@users.noreply.github.com>
2021-08-28 11:22:00 +0530

    Add files via upload

---
+++ Python/Activities/Activity15.py
@@ -0,0 +1,5 @@
+variable='x'
+try:
+    print(x)
+except NameError:
+    print("x hasn't been defined yet.")
---
+++ Python/Activities/ClassExample.py
@@ -0,0 +1,6 @@
+class MyClass:
+  'This is an example class'
+  x = 5
+  
+p1 = MyClass()
+print(p1.x)
