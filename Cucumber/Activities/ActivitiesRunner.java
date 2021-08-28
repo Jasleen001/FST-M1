98966c455763a6511a7ee6e3a965c5a779c9f5d4
Jasleen001 <88023368+Jasleen001@users.noreply.github.com>
2021-08-28 22:56:01 +0530

    Add files via upload

---
+++ Cucumber/Activities/ActivitiesRunner.java
@@ -0,0 +1,19 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	    features = "Features",
+	    glue = {"stepDefinitions"},
+	    tags = "@SmokeTest",
+	    plugin = {"pretty"},
+	    monochrome = true
+	)
+
+
+public class ActivitiesRunner {
+
+}
